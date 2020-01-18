d9391e33cd0819a640438ed8652e0464e29d75eb
Max Ringelheim <max.ringelheim@gmail.com>
2020-01-18 20:23:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@ board = [" O ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   "]
 def display_board(board)
   puts "#{board[0]}|   |   "
   puts "-----------"
-  puts "   |#{board[4]}   |   "
+  puts "   |#{board[4]}|   "
   puts "-----------"
   puts "   |   |   "
 end
